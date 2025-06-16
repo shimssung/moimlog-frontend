61afa9db5c693aa65f30536251be9e0e43b94d55
shimssung <shimssung@gmail.com>
2025-06-17 00:07:39 +0900

    feacator: CSP설정에서 이미지 미리보기를 위한 blob설정 추가

--- next.config.js
+++ next.config.js
@@ -10,21 +10,8 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval';
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: https:;
-              font-src 'self';
-              object-src 'none';
-              base-uri 'self';
-              form-action 'self';
-              frame-ancestors 'none';
-              block-all-mixed-content;
-              upgrade-insecure-requests;
-            `
-              .replace(/\s+/g, " ")
-              .trim(),
+            value:
+              "default-src 'self'; img-src 'self' data: https: blob:; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
           },
           {
             key: "X-Content-Type-Options",
