c3811515bc4bbc01da5b72d0b8354fc7fc6c758a
Yunsung <shimssung@gmail.com>
2025-07-14 19:33:00 +0900

    refactor: SSR 깜빡임 해결

---
+++ src/pages/_document.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-unused-vars */
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+              ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => {
+            return sheet.collectStyles(<App {...props} />)
+          },
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function() {
+                  try {
+                    var theme = localStorage.getItem('moimlog-storage');
+                    if (theme) {
+                      var parsed = JSON.parse(theme);
+                      if (parsed.isDarkMode) {
+                        document.documentElement.classList.add('dark-mode');
+                      }
+                    }
+                  } catch (e) {}
+                })();
+              `,
+            }}
+          />
+        </body>
+      </Html>
+    )
+  }
+} 
